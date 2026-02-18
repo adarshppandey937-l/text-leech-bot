1cd47dc0faba9af39e759520c0bce2289ce9d684
adarshppandey937-l <adarshppandey937@gmail.com>
2026-02-18 08:07:16 +0530

    Change API_ID, API_HASH, and BOT_TOKEN values
    Updated API credentials for the application.

--- modules/vars.py
+++ modules/vars.py
@@ -2,9 +2,9 @@
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
-API_ID    = os.environ.get("API_ID", "23211113")
-API_HASH  = os.environ.get("API_HASH", "b0e01552398f684f9071fe4c490ee781")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+API_ID    = os.environ.get("API_ID", "22865155")
+API_HASH  = os.environ.get("API_HASH", "e430e3f61712616b926be959f1612c46")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8311285737:AAH1oqlu-slXE9P722z-Boa8yo2eR4kzNJI") 
 
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
